257b05329f4e0ba943a62260ead6f4590c9cf3e9
Christian Salgård Svinding <chri30cm@edu.eal.dk>
2018-09-30 11:53:34 +0200

    Jeg har lavet nogle ændringer i Smartmenu.cs som jeg synes er lidt bedre :D prøv det af. kan nemt lave det om igen. men tror stadig at vi skal have lavet det helt om med switch så det vil jeg lige prøve lidt senere hvis ikke i har det.

--- SmartMenuLibrary/SmartMenu.cs
+++ SmartMenuLibrary/SmartMenu.cs
@@ -16,100 +16,129 @@ namespace SmartMenuLibrary
 
         public void Activate()
         {
+            string line;
+            int counter = 0;
+
             Console.WriteLine("For english press 1 and enter - for dansk tryk 2 og enter - 0 for exit ");
 
             //char menuinput er character input - det som bliver indskrevet
             char menuInput;
-            menuInput = char.Parse(Console.ReadLine());
-            string line;
-            int counter = 0;
             char menuInputEN;
             char menuInputDK;
 
+            menuInput = char.Parse(Console.ReadLine());
+
             //vælger punkt 1
-            while (menuInput == '1')
+            if (menuInput == '1')
             {
                 //indlæser engelsk tekstfil med metoder vi lærte i mandags
                 System.IO.StreamReader file = new System.IO.StreamReader(@"C:..\..\MenuSpec.txt");
-                while((line = file.ReadLine()) != null)
-                {
-                    Console.WriteLine(line);
-                        counter++;
-                }
-                //herunder vælger den hvad outputtet er alt efter hvad indputtet er
-                menuInputEN = char.Parse(Console.ReadLine());
 
-                //indputtet er 1 og outputter er dothis fra functions - lige nu looper den bare for evigt though lol
-                while (menuInputEN == '1')
-                {
-                     Console.WriteLine(Functions.DoThis());
-                }
-
-                while (menuInputEN == '2')
-                {
-                    Console.WriteLine(Functions.DoThat());
-                }
-
-                while (menuInputEN == '3')
-                {
-                    //den her skal have et string indput for at fungere, derfor er der først defineret en string som læser det som man indtaster 
-                    string doSomethingInput = Console.ReadLine();
-                    Console.WriteLine(Functions.DoSomething(doSomethingInput));
-                }
-
-                while (menuInputEN == '4')
-                {
-                    Console.WriteLine(Functions.GetTheAnswerToLifeTheUniverseAndEverything());
-                }
-
-                while (menuInputEN == '0')
-                {
-                    //afslutter programmet
-                    Environment.Exit(0);
-                }
-            }
-
-            //præcis det samme som øverst, bare repeat på dansk
-            while (menuInput == '2')
-            {
-                System.IO.StreamReader file = new System.IO.StreamReader(@"C:..\..\MenuSpecDA.txt");
                 while ((line = file.ReadLine()) != null)
                 {
                     Console.WriteLine(line);
                     counter++;
                 }
-                menuInputDK = char.Parse(Console.ReadLine());
 
-                while (menuInputDK == '1')
+                menuInputEN = char.Parse(Console.ReadLine());
+
+                //herunder vælger den hvad outputtet er alt efter hvad indputtet er
+
+                //indputtet er 1 og outputter er dothis fra functions - lige nu looper den bare for evigt though lol
+                if (menuInputEN == '1')
                 {
+                    Console.Clear();
                     Console.WriteLine(Functions.DoThis());
+                    Console.ReadLine();
                 }
 
-                while (menuInputDK == '2')
+                if (menuInputEN == '2')
                 {
+                    Console.Clear();
                     Console.WriteLine(Functions.DoThat());
+                    Console.ReadLine();
                 }
 
-                while (menuInputDK == '3')
+                if (menuInputEN == '3')
                 {
+                    Console.Clear();
+                    //den her skal have et string indput for at fungere, derfor er der først defineret en string som læser det som man indtaster 
                     string doSomethingInput = Console.ReadLine();
                     Console.WriteLine(Functions.DoSomething(doSomethingInput));
+                    Console.ReadLine();
                 }
 
-                while (menuInputDK == '4')
+                if (menuInputEN == '4')
                 {
+                    Console.Clear();
                     Console.WriteLine(Functions.GetTheAnswerToLifeTheUniverseAndEverything());
+                    Console.ReadLine();
                 }
 
-                while (menuInputDK == '0')
+                if (menuInputEN == '0')
                 {
+                    Console.Clear();
+                    //afslutter programmet
                     Environment.Exit(0);
+                    Console.ReadLine();
                 }
-            
+            }
 
-            while (menuInput == '0')
+            //præcis det samme som øverst, bare repeat på dansk
+            if (menuInput == '2')
+            {
+                System.IO.StreamReader file = new System.IO.StreamReader(@"C:..\..\MenuSpecDA.txt");
+
+                while ((line = file.ReadLine()) != null)
                 {
+                    Console.WriteLine(line);
+                    counter++;
+                }
+
+                menuInputDK = char.Parse(Console.ReadLine());
+
+                if (menuInputDK == '1')
+                {
+                    Console.Clear();
+                    Console.WriteLine(Functions.DoThis());
+                    Console.ReadLine();
+                }
+
+                if (menuInputDK == '2')
+                {
+                    Console.Clear();
+                    Console.WriteLine(Functions.DoThat());
+                    Console.ReadLine();
+                }
+
+                if (menuInputDK == '3')
+                {
+                    Console.Clear();
+                    string doSomethingInput = Console.ReadLine();
+                    Console.WriteLine(Functions.DoSomething(doSomethingInput));
+                    Console.ReadLine();
+                }
+
+                if (menuInputDK == '4')
+                {
+                    Console.Clear();
+                    Console.WriteLine(Functions.GetTheAnswerToLifeTheUniverseAndEverything());
+                    Console.ReadLine();
+                }
+
+                if (menuInputDK == '0')
+                {
+                    Console.Clear();
                     Environment.Exit(0);
+                    Console.ReadLine();
+                }
+
+
+                if (menuInput == '0')
+                {
+                    Console.Clear();
+                    Environment.Exit(0);
+                    Console.ReadLine();
                 }
             }
 
