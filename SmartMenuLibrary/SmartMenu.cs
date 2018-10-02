cab7c98e677067a0b8da156aa4d5f2e8c81e18cd
Christian Salgård Svinding <chri30cm@edu.eal.dk>
2018-10-02 13:10:18 +0200

    har fixet

--- SmartMenuLibrary/SmartMenu.cs
+++ SmartMenuLibrary/SmartMenu.cs
@@ -28,127 +28,130 @@ namespace SmartMenuLibrary
 
             menuInput = char.Parse(Console.ReadLine());
 
-            while (menuInput == '1')
+            while (menuInput != '0')
             {
-                Console.Clear();
-                System.IO.StreamReader file = 
-                       new System.IO.StreamReader(@"..\..\MenuSpec.txt");
-
-                while ((line = file.ReadLine()) != null)
-                {
-                    Console.WriteLine(line);
-                    counter++;
-                }
-
-                menuInputEN = char.Parse(Console.ReadLine());
-
-                if (menuInputEN == '1')
+                while (menuInput == '1')
                 {
                     Console.Clear();
-                    Console.WriteLine(Functions.DoThis());
-                    Console.WriteLine("\nPress enter to go back");
-                    Console.ReadLine();
+                    System.IO.StreamReader file =
+                           new System.IO.StreamReader(@"..\..\MenuSpec.txt");
+
+                    while ((line = file.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                        counter++;
+                    }
+
+                    menuInputEN = char.Parse(Console.ReadLine());
+
+                    if (menuInputEN == '1')
+                    {
+                        Console.Clear();
+                        Console.WriteLine(Functions.DoThis());
+                        Console.WriteLine("\nPress enter to go back");
+                        Console.ReadLine();
+                    }
+
+                    else if (menuInputEN == '2')
+                    {
+                        Console.Clear();
+                        Console.WriteLine(Functions.DoThat());
+                        Console.WriteLine("\nPress enter to go back");
+                        Console.ReadLine();
+                    }
+
+                    else if (menuInputEN == '3')
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Do something");
+                        string doSomethingInput = Console.ReadLine();
+                        Console.WriteLine(Functions.DoSomething(doSomethingInput));
+                        Console.WriteLine("\nPress enter to go back");
+                        Console.ReadLine();
+                    }
+
+                    else if (menuInputEN == '4')
+                    {
+                        Console.Clear();
+                        Console.WriteLine(Functions.GetTheAnswerToLifeTheUniverseAndEverything());
+                        Console.WriteLine("\nPress enter to go back");
+                        Console.ReadLine();
+                    }
+
+                    else if (menuInputEN == '5')
+                    {
+                        Console.Clear();
+                        Console.WriteLine("For english press 1 and enter - for dansk tryk 2 og enter - 0 for exit ");
+                        menuInput = char.Parse(Console.ReadLine());
+                    }
+
+                    else if (menuInputEN == '0')
+                    {
+                        Console.Clear();
+                        Environment.Exit(0);
+                    }
                 }
 
-                else if (menuInputEN == '2')
+                while (menuInput == '2')
                 {
                     Console.Clear();
-                    Console.WriteLine(Functions.DoThat());
-                    Console.WriteLine("\nPress enter to go back");
-                    Console.ReadLine();
-                }
+                    System.IO.StreamReader file =
+                        new System.IO.StreamReader(@"..\..\MenuSpecDA.txt");
 
-                else if (menuInputEN == '3')
-                {
-                    Console.Clear();
-                    Console.WriteLine("Do something");
-                    string doSomethingInput = Console.ReadLine();
-                    Console.WriteLine(Functions.DoSomething(doSomethingInput));
-                    Console.WriteLine("\nPress enter to go back");
-                    Console.ReadLine();
-                }
+                    while ((line = file.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                        counter++;
+                    }
 
-                else if (menuInputEN == '4')
-                {
-                    Console.Clear();
-                    Console.WriteLine(Functions.GetTheAnswerToLifeTheUniverseAndEverything());
-                    Console.WriteLine("\nPress enter to go back");
-                    Console.ReadLine();
-                }
+                    menuInputDK = char.Parse(Console.ReadLine());
 
-                else if (menuInputEN == '5')
-                {
-                    Console.Clear();
-                    Console.WriteLine("For english press 1 and enter - for dansk tryk 2 og enter - 0 for exit ");
-                    menuInput = char.Parse(Console.ReadLine());
-                }
+                    if (menuInputDK == '1')
+                    {
+                        Console.Clear();
+                        Console.WriteLine(Functions.DoThis());
+                        Console.WriteLine("\nTryk enter for at gå tilbage");
+                        Console.ReadLine();
+                    }
 
-                else if (menuInputEN == '0')
-                {
-                    Console.Clear();
-                    Environment.Exit(0);
-                }
-            }
+                    else if (menuInputDK == '2')
+                    {
+                        Console.Clear();
+                        Console.WriteLine(Functions.DoThat());
+                        Console.WriteLine("\nTryk enter for at gå tilbage");
+                        Console.ReadLine();
+                    }
 
-            while (menuInput == '2')
-            {
-                Console.Clear();
-                System.IO.StreamReader file = 
-                    new System.IO.StreamReader(@"..\..\MenuSpecDA.txt");
+                    else if (menuInputDK == '3')
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Gør noget");
+                        string doSomethingInput = Console.ReadLine();
+                        Console.WriteLine(Functions.DoSomething(doSomethingInput));
+                        Console.WriteLine("\nTryk enter for at gå tilbage");
+                        Console.ReadLine();
+                    }
 
-                while ((line = file.ReadLine()) != null)
-                {
-                    Console.WriteLine(line);
-                    counter++;
-                }
+                    else if (menuInputDK == '4')
+                    {
+                        Console.Clear();
+                        Console.WriteLine(Functions.GetTheAnswerToLifeTheUniverseAndEverything());
+                        Console.WriteLine("\nTryk enter for at gå tilbage");
+                        Console.ReadLine();
+                    }
 
-                menuInputDK = char.Parse(Console.ReadLine());
+                    else if (menuInputDK == '5')
+                    {
+                        Console.Clear();
+                        Console.WriteLine("For english press 1 and enter - for dansk tryk 2 og enter - 0 for exit ");
+                        menuInput = char.Parse(Console.ReadLine());
+                    }
 
-                if (menuInputDK == '1')
-                {
-                    Console.Clear();
-                    Console.WriteLine(Functions.DoThis());
-                    Console.WriteLine("\nTryk enter for at gå tilbage");
-                    Console.ReadLine();
-                }
-
-                else if (menuInputDK == '2')
-                {
-                    Console.Clear();
-                    Console.WriteLine(Functions.DoThat());
-                    Console.WriteLine("\nTryk enter for at gå tilbage");
-                    Console.ReadLine();
-                }
-
-                else if (menuInputDK == '3')
-                {
-                    Console.Clear();
-                    Console.WriteLine("Gør noget");
-                    string doSomethingInput = Console.ReadLine();
-                    Console.WriteLine(Functions.DoSomething(doSomethingInput));
-                    Console.WriteLine("\nTryk enter for at gå tilbage");
-                    Console.ReadLine();
-                }
-
-                else if (menuInputDK == '4')
-                {
-                    Console.Clear();
-                    Console.WriteLine(Functions.GetTheAnswerToLifeTheUniverseAndEverything());
-                    Console.WriteLine("\nTryk enter for at gå tilbage");
-                    Console.ReadLine();
-                }
-
-                else if (menuInputDK == '5')
-                {
-                    Console.Clear();
-                    Console.WriteLine("For english press 1 and enter - for dansk tryk 2 og enter - 0 for exit ");
-                    menuInput = char.Parse(Console.ReadLine());
-                }
-
-                else if (menuInputDK == '0')
-                {
-                    Console.Clear();
-                    Environment.Exit(0);
+                    else if (menuInputDK == '0')
+                    {
+                        Console.Clear();
+                        Environment.Exit(0);
+                    }
                 }
             }
         }
